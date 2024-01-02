f50ceb6b50a0ecb3c64bae40be0b824b4fb355b6
vrundadhamsania <155124170+vrundadhamsania@users.noreply.github.com>
2024-01-02 10:47:17 +0530

    Add files via upload

---
+++ mycatch.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "firstname:".$_firstname=$_REQUEST['fname'];
+echo "lastname:".$_larstname=$_REQUEST['lname'];
+echo "cityname:".$_firstname=$_REQUEST['cname'];
+echo "mobilename:".$_mobilename=$_REQUEST['mname'];
+
+?>
---
+++ vrunda.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+registeration from
+</head>
+<boby>
+<form action="mycatch.php" method="post">
+first name<input type="text" name="fname" />
+<br>
+last name<input type="text" name="lname" />
+<br>
+city name<input type="text" name="cname" />
+<br>
+mobile<input type="text" name="mname" />
+<br>
+division<input type="text" name="dname" />
+<br>
+rollno<input type="text" name="rname" />
+<br>
+department<input type="text" name="deptname" />
+<br>
+</form>
+</body>
+</html>
